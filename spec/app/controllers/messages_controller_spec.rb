46c0c02441564006442907c2ecf02ffb574148c5
Jarrett Meyer <jarrettmeyer@gmail.com>
2012-11-01 05:34:25 -0400

    added spec to test updating the chatroom

--- spec/app/controllers/messages_controller_spec.rb
+++ spec/app/controllers/messages_controller_spec.rb
@@ -6,9 +6,18 @@ describe MessagesController, type: :controller do
 	describe 'GET messages' do 
 
 		it "returns a JSON object" do
-			get :index, { room_key: "xyz987" }
+			get :index, { room_key: "" }
 			json = JSON.parse response.body
-			json.should_not be_nil
+			json.should == []
+		end
+
+		it "should update the chatroom" do
+			chatroom = create_chatroom
+			original_updated_at = chatroom.updated_at
+			get :index, { room_key: chatroom.room_key }
+			
+			new_chatroom = Chatroom.find chatroom.id
+			new_chatroom.updated_at.should be > original_updated_at
 		end
 
 	end
