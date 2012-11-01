1ca4631da5369766683621ec2d3611f716ca42c7
Jarrett Meyer <jarrettmeyer@gmail.com>
2012-11-01 05:47:36 -0400

    added specs for create

--- spec/app/controllers/messages_controller_spec.rb
+++ spec/app/controllers/messages_controller_spec.rb
@@ -20,6 +20,33 @@ describe MessagesController, type: :controller do
 			new_chatroom.updated_at.should be > original_updated_at
 		end
 
+		it "returns an OK status" do
+			get :index, room_key: ""
+			response.status.should == 200 # 200 = OK
+		end
+
+	end
+
+	describe 'POST /messages' do
+
+		it "should fetch a chatroom" do
+			chatroom = create_chatroom
+			post :create, room_key: chatroom.room_key, display_name: "John Doe", text: "Hello, World!"
+			assigns(:chatroom).should == chatroom
+		end
+
+		it "should add a message to the chatroom" do
+			chatroom = create_chatroom
+			post :create, room_key: chatroom.room_key, display_name: "John Doe", text: "Hello, World!"
+			assigns(:chatroom).messages.count.should == 1
+		end
+
+		it "returns a created status" do
+			chatroom = create_chatroom
+			post :create, room_key: chatroom.room_key, display_name: "John Doe", text: "Hello, World!"
+			response.status.should == 201 # 201 = created
+		end
+
 	end
 
 	def create_chatroom
